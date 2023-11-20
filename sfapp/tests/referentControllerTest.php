4f1d29b523a0d6226d4f06ee8f05e63a360c576d
Ulysse Gay <ugay@ssd14.univ-lr.fr>
2023-11-20 12:11:21 +0100

    [ADD] Debut des tests

--- sfapp/tests/referentControllerTest.php
+++ sfapp/tests/referentControllerTest.php
@@ -5,6 +5,9 @@ namespace App\Tests;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\AbstractBrowser;
 
+use App\Entity\SA;
+use Doctrine\Persistence\ManagerRegistry;
+
 class referentControllerTest extends WebTestCase
 {
     public function testSAPage()/*Test de l'affichage de la page*/
@@ -15,4 +18,27 @@ class referentControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(200);
     }
+
+    public function testReferentPage() // Test de l'affichage de la page d'accueil du référent
+    {
+        $client = static::createClient();
+        $client->request('GET', '/referent');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testChangeRoomPage() // Test de l'affichage de la page d'accueil du référent
+    {
+        $doctrine = new ManagerRegistry();
+        $sa = new SA();
+
+        $client = static::createClient();
+        $client->request('GET', '/referent/changersalle/'.$sa->getId());
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+
 }
