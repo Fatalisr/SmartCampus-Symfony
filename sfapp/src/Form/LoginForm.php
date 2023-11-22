88505df47ffb9e6e39208deaafa078113f52b50f
Gabin Joyet <gjoyet@dimm10.univ-lr.fr>
2023-11-22 08:50:32 +0100

    [FIX] Form return type

--- sfapp/src/Form/LoginForm.php
+++ sfapp/src/Form/LoginForm.php
@@ -11,7 +11,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class LoginForm extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
     {
         $builder
             ->add('username', TextType::class, [
