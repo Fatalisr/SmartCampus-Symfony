4785065d4fa056416d992ebd976a32f5d0e486b2
Ulysse Gay <ugay@dimm08.univ-lr.fr>
2023-11-22 11:56:52 +0100

    [ADD] Fin de séance

--- sfapp/tests/SARepositoryTest.php
+++ sfapp/tests/SARepositoryTest.php
@@ -1,6 +1,55 @@
 <?php
 
-class SARepositoryTest
-{
+// tests/Repository/ProductRepositoryTest.php
 
+namespace App\Tests\Repository;
+
+use App\Entity\SA;
+use App\Entity\Room;
+use App\Repository\SARepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SARepositoryTest extends KernelTestCase
+{
+    private \Doctrine\ORM\EntityManager $entityManager;
+
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+
+    $this->entityManager = $kernel->getContainer()
+        ->get('doctrine')
+        ->getManager();
+    }
+
+    public function testFindAllPlanAction(): void
+    {
+
+
+        $products = $this->entityManager
+        ->getRepository(SA::class)
+        ->findAllPlanAction();
+
+        $this->assertCount(3,$products);
+    }
+
+    public function testFindAllInactive(): void
+    {
+
+        $products = $this->entityManager
+            ->getRepository(SA::class)
+            ->findAllInactive();
+
+        $this->assertCount(1,$products);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
 }
--- sfapp/tests/referentControllerTest.php
+++ sfapp/tests/referentControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests;
 
+use App\Entity\SA;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\BrowserKit\AbstractBrowser;
 
@@ -10,9 +11,28 @@ class referentControllerTest extends WebTestCase
     public function testSAPage()/*Test de l'affichage de la page*/
     {
         $client = static::createClient();
-        $client->request('GET', '/referent/sa/1');
+        $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
+
+        // Suppression des instances de test pour eviter les conflit dans la bdd
+        $entityManager->beginTransaction(); // Begin a transaction
+        $entityManager->createQuery("DELETE FROM App\Entity\SA S WHERE S.name=:nom")->setParameter('nom','Test_SA')->execute();
+        $entityManager->commit();
+
+        $sa = new SA();
+        $sa->setName('Test_SA');
+        $sa->setState('ACTIF');
+        $entityManager->persist($sa);
+        $entityManager->flush();
+
+
+        $client->request('GET', '/referent/sa/'.$sa->getId());
 
         $this->assertResponseIsSuccessful();
         $this->assertResponseStatusCodeSame(200);
+
+        // Suppression des instances de test pour eviter les conflit dans la bdd
+        $entityManager->beginTransaction(); // Begin a transaction
+        $entityManager->createQuery("DELETE FROM App\Entity\SA S WHERE S.name=:nom")->setParameter('nom','Test_SA')->execute();
+        $entityManager->commit();
     }
 }
