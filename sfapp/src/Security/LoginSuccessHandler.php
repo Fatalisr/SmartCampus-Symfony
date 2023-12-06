5110530db543e582354b8aee5cc5e5fbd3879526
Romain Moreau <romain.moreau@etudiant.univ-lr.fr>
2023-12-06 22:54:35 +0100

    [ADD] Route vers technicien aussi fonctionelle

---
+++ sfapp/src/Security/LoginSuccessHandler.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Security;
+
+use ContainerVbuOxp0\getConsole_ErrorListenerService;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): ?\Symfony\Component\HttpFoundation\Response
+    {
+        $user = $token->getUser();
+
+        if(in_array("ROLE_REFERENT", $user->getRoles()))
+        {
+            return new RedirectResponse('/referent');
+        }
+        else if(in_array("ROLE_TECHNICIEN", $user->getRoles()))
+        {
+            return new RedirectResponse('/technicien');
+        }
+        else
+        {
+            return new RedirectResponse('/');        }
+    }
+
+
+}
