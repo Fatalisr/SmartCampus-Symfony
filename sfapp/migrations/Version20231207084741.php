55be3409e380d0a505706942d1bb108e4d18e3e2
Romain Moreau <rmorea05@dimm11.univ-lr.fr>
2023-12-07 10:03:33 +0100

    [ADD] add dans les fixtures

--- sfapp/migrations/Version20231207084741.php
+++ sfapp/migrations/Version20231207084741.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231206125845 extends AbstractMigration
+final class Version20231207084741 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,18 @@ final class Version20231206125845 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE member CHANGE role role VARCHAR(15) NOT NULL check (role in (\'REFERENT\',\'TECHNICIEN\'))');
         $this->addSql('ALTER TABLE room CHANGE facing facing VARCHAR(1) NOT NULL check (facing in (\'N\',\'S\',\'E\',\'W\'))');
         $this->addSql('ALTER TABLE sa CHANGE state state VARCHAR(15) NOT NULL check (state in (\'INACTIF\',\'ACTIF\',\'MAINTENANCE\',\'A_INSTALLER\'))');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL COMMENT \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE member CHANGE role role VARCHAR(15) NOT NULL');
+        $this->addSql('DROP TABLE user');
         $this->addSql('ALTER TABLE room CHANGE facing facing VARCHAR(1) NOT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL COMMENT \'(DC2Type:json)\'');
         $this->addSql('ALTER TABLE sa CHANGE state state VARCHAR(15) NOT NULL');
+        $this->addSql('ALTER TABLE member CHANGE role role VARCHAR(15) NOT NULL');
     }
 }
