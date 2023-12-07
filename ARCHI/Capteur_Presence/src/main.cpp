8ad250046f42b4e43040def58ed5ada38c660891
Romain Moreau <romain.moreau@etudiant.univ-lr.fr>
2023-12-07 10:59:29 +0100

    [ADD] Fonction pour récuperer si il y a du mouvement

---
+++ ARCHI/Capteur_Presence/src/main.cpp
@@ -0,0 +1,41 @@
+// main.cpp
+#include <Arduino.h>
+
+#define digital_pir_sensor 5 // connect to Pin 5 your motion sensor
+
+/*
+ * @brief Read the value from the sensor
+ * @return 1 if there is a presence detected else return 0.
+ */
+bool get_Motion_value();
+
+
+/*
+ * @brief Setup of the sensor
+*/
+void setup()
+{
+  Serial.begin(115200);  // set baud rate as 115200
+  pinMode(digital_pir_sensor,INPUT); // set Pin mode as input
+}
+
+
+/*
+ * @brief Loop where the sensor will be stuck in
+*/
+void loop()
+{
+  bool state = get_Motion_value(); // take if there is a motion
+  if (state == 1)
+    Serial.println("A Motion has occured");  // When there is a response
+  else if (state == 0)
+    Serial.println("Nothing Happened");  // Far from PIR sensor
+  delay(1000);
+}
+
+
+bool get_Motion_value()
+{
+  bool state = digitalRead(digital_pir_sensor); // read from PIR sensor
+  return state; // return 1 if a motion is detected return 0 else
+}
