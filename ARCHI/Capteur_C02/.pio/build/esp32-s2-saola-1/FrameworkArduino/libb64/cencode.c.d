a254a7e024c6193fad251363317e239ed2a1c963
Gabin Joyet <gjoyet@dimm10.univ-lr.fr>
2023-12-07 12:03:11 +0100

    [ADD] Disabled button

--- ARCHI/Capteur_C02/.pio/build/esp32-s2-saola-1/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32-s2-saola-1/FrameworkArduino/libb64/cencode.c.o: \
- /Users/gjoyet/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /Users/gjoyet/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
--- ARCHI/Capteur_C02/.pio/build/esp32-s2-saola-1/liba1c/Grove SGP30_Gas_Sensor/sensirion_common.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32-s2-saola-1/liba1c/Grove\ SGP30_Gas_Sensor/sensirion_common.c.o: \
- .pio/libdeps/esp32-s2-saola-1/Grove\ SGP30_Gas_Sensor/sensirion_common.c \
- .pio/libdeps/esp32-s2-saola-1/Grove\ SGP30_Gas_Sensor/sensirion_common.h \
- .pio/libdeps/esp32-s2-saola-1/Grove\ SGP30_Gas_Sensor/sensirion_configuration.h
