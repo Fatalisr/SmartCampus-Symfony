6bdbae09ffe12684a60c9d0c9d2c9e4dcca3d007
Mathieu Blandin <mblandin@dimm11.univ-lr.fr>
2023-12-19 14:36:43 +0100

    [ADD] Nouvelle version avec la bonne classe

--- ARCHI/Capteur_Humidite_Temperature/src/main.cpp
+++ ARCHI/Capteur_Humidite_Temperature/src/main.cpp
@@ -6,8 +6,7 @@
 #ifdef U8X8_HAVE_HW_I2C
 #include <Wire.h>
 #endif
-#include <DHT.h> //HUMI-TEMP
-#include <Adafruit_Sensor.h>
+#include "DHTesp.h"
 
 /*
  * Initialisation of the global variables
@@ -16,27 +15,16 @@
 //Screen
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C screen(U8G2_R0, /* clock=*/ SCL, /*data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
 
-#define DHT_PIN 10
-#define DHT_TYPE DHT22
-DHT humiTemp(DHT_PIN, DHT_TYPE); //Temperature and Humidity Sensor
+DHTesp dht;
 
-/*
- * @brief Read the Humidity value from the sensor
- * @return the humidity detected. Betwenn 5% and 99%.
- */
-float get_Humidity_value();
+/** Pin number for DHT22 data pin */
+int dhtPin = 17;
 
-/*
- * @brief Read the Temperature value from the sensor
- * @return the temperature detected. Betwenn -40°C and 80°C.
- */
-float get_Temperature_value();
+String Temp;
+String Humid;
 
-/*
- * @brief display on screen
- * Add the given value to the screen buffer at the given coordinates
- */
-void print_Screen(int x, int y, float value);
+bool getTemperature();
+bool initTemp();
 
 /*
  * @brief Setup of the sensor
@@ -45,47 +33,70 @@ void setup() {
   Serial.begin(9600);
   screen.begin();
   screen.setFont(u8g2_font_luBIS08_tf);
-  if(!humiTemp.begin()){ //Verify if the connection has been established
-    Serial.println("Erreur lors du demarrage du capteur !");
-  }
+
+  Serial.println();
+  Serial.println("DHT ESP32 example with tasks");
+
+  initTemp();
 }
 
 /*
  * @brief Loop where the sensor will be stuck in
 */
 void loop(){
-  float Temp, Humid;
-  Temp = get_Temperature_value();
-  Humid = get_Humidity_value();
-
-  print_Screen(5, 10, Temp);
-  print_Screen(5, 30, Humid);
+  if(getTemperature())
+  {
+    screen.drawStr(10, 10, Temp.c_str());
+    screen.drawStr(10, 30, Humid.c_str());
+  }
 
   screen.sendBuffer(); //Sending the buffer to the screen
-
   screen.clearBuffer(); //Clearing the buffer
 
   delay(3000); //3 second delay
 }
 
-void get_Humidity_value(){
-  float humidity = humiTemp.readHumidity();
-  if (isnan(humidity)) { //Verify if a value has been returned
-      Serial.println("Erreur lors de la lecture du capteur DHT !");
-      delay(1000);
+/**
+ * initTemp
+ * Setup DHT library
+ * Setup task and timer for repeated measurement
+ * @return bool
+ *    true if DHT22 is started
+ *    false if DHT22 couldn't be started
+ */
+bool initTemp() {
+  // Initialize temperature sensor
+	dht.setup(dhtPin, DHTesp::DHT22);
+  if (dht.getStatus() != 0) {
+		Serial.println("DHT22 error status: " + String(dht.getStatusString()));
+		return false;
   }
+	Serial.println("DHT22 initiated");
+
+  return true;
 }
 
-void get_Temperature_value(){
-  float temp = humiTemp.readTemperature();
-  if (isnan(temp)) { //Verify if a value has been returned
-      Serial.println("Erreur lors de la lecture du capteur DHT !");
-      delay(1000);
-  }
-}
+/**
+ * getTemperature
+ * Reads temperature from DHT22 sensor
+ * @return bool
+ *    true if temperature could be aquired
+ *    false if aquisition failed
+*/
+bool getTemperature() {
+	// Reading temperature for humidity takes about 250 milliseconds!
+	// Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
+  TempAndHumidity newValues = dht.getTempAndHumidity();
 
-void print_Screen(int x, int y, float value){
-  char message[20];
-  sprintf(message, "%f", value); //Changing float into string and affecting it onto message
-  screen.drawStr(x, y, message); //Adding to the screen buffer
+	// Check if any reads failed and exit early (to try again).
+	if (dht.getStatus() != 0) {
+		Serial.println("DHT22 error status: " + String(dht.getStatusString()));
+		return false;
+  }
+
+  Temp = "Temperature : " + String(newValues.temperature);
+  Humid = "Humidite : " + String(newValues.humidity);
+  Temp.remove(Temp.length()-1);
+  Humid.remove(Humid.length()-1);
+	return true;
 }
