ea400aee780e737195f83861859de2c822128cdb
Ulysse Gay <ugay@dimm08.univ-lr.fr>
2023-12-21 10:01:57 +0100

    [ADD] Query sur l'intervalle de temps

--- sfapp/src/Service/ConnexionRequetesAPI.php
+++ sfapp/src/Service/ConnexionRequetesAPI.php
@@ -34,14 +34,14 @@ class ConnexionRequetesAPI
         if($response->getStatusCode() == 200)   // Checks if the request was successful (200 indicates that the request was successful)
         {
             return $response->getContent();
+            //return json_decode($response->getContent());
         }
 
-        return $response->getContent();
-        //return $response->getStatusCode();
+        return $response->getStatusCode();
         //return $response->getInfo();
     }
 
-    public function postCaptures(array $requestJson) : array
+    public function postCaptures()
     {
         
         $response = $this->client->request(                         // Creates and sends the request to the API
@@ -53,13 +53,15 @@ class ConnexionRequetesAPI
                 'dbname' => 'sae34bdm1eq1',                         // Informing the name of our database
                 'username' => 'm1eq1',                              // Informing the username to connect to our database
                 'userpass' => 'sodqif-vefXym-0cikho',               // Informing the password to connect to our database
-
             ],
-            'body' => $requestJson,
+                // 'body' => $requestJson,
+                'json' => ['id' => '51', 'nom' => 'co2', 'valeur' => '402', 'dateCapture' => '2022-11-28 08:00:00', 'localisation' => 'D004', 'description' => '', 'tag' => '13'],
         ]);
 
         if (201 != $response->getStatusCode()) {
-            throw new Exception('Response status code is different than expected.');
+            //throw new Exception('Response status code is different than expected.');
+            //return $response->getInfo('debug');
+            return $response->getStatusCode();
         }
 
         if($response->getStatusCode() == 201)   // Checks if the request was successful (201 indicates that the request was successful)
@@ -71,7 +73,7 @@ class ConnexionRequetesAPI
         //return $response->getInfo();
     }
 
-    public function getIntervalCaptures()
+    public function getIntervalCaptures($date1,$date2)
     {
 
         $response = $this->client->request(                                 // Creates and sends the request to the API
@@ -81,7 +83,10 @@ class ConnexionRequetesAPI
                 'dbname' => 'sae34bdm1eq1',                                 // Informing the name of our database
                 'username' => 'm1eq1',                                      // Informing the username to connect to our database
                 'userpass' => 'sodqif-vefXym-0cikho',                       // Informing the password to connect to our database
-
+                'query' => [
+                    'date1' => $date1,
+                    'date2' => $date2,
+                ]
             ],
         ]);
 
@@ -90,8 +95,8 @@ class ConnexionRequetesAPI
             return $response->getContent();
         }
 
-        return $response->getContent();
-        //return $response->getStatusCode();
+        //return $response->getContent();
+        return $response->getStatusCode();
         //return $response->getInfo();
     }
 
