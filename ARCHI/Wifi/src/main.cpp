15f21bd0d2c3c72b2d3b123a4ef29036b2e6e96d
Gabin Joyet <gjoyet@dimm10.univ-lr.fr>
2023-12-22 15:28:53 +0100

    [ADD] POST request

--- ARCHI/Wifi/src/main.cpp
+++ ARCHI/Wifi/src/main.cpp
@@ -1,91 +1,51 @@
 #include <Arduino.h>
-#include <WiFiClientSecure.h> //Wifi library
+#include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <ArduinoHttpClient.h>
+#include "esp_wpa2.h" //Librairie wpa2 pour la connexion au réseaux d'enterprise
 
-#include "esp_wpa2.h" //wpa2 library for connections to Enterprise networks
-#define EAP_IDENTITY "gjoyet" //if connecting from another corporation, use identity@organisation.domain in Eduroam
-#define EAP_USERNAME "gjoyet" //oftentimes just a repeat of the identity
-#define EAP_PASSWORD "Ztitbw(002)googlegab" //your Eduroam password
+#define EAP_IDENTITY "ugay" //Login
+#define EAP_USERNAME "ugay" //Login
+#define EAP_PASSWORD "LaceulalTelephone7!" //Eduroam password
 const char* ssid = "eduroam"; // Eduroam SSID
 
-WiFiClientSecure Wifi;
+const String serverName = "https://sae34.k8s.iut-larochelle.fr/api/captures"; //URL de l'api pour l'envoie de données
+int counter;//Compteur pour la connexion
+unsigned long lastTime = 0;//Compteur pour le délai entre chaque envoie
+unsigned long timerDelay = 15000;//Délai de 30 secondes
 
-//Your Domain name with URL path or IP address with path
-char serverName[] = "https://sae34.k8s.iut-larochelle.fr/api/captures";
-char certificat[] = "-----BEGIN CERTIFICATE-----\n" \
-"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-"-----END CERTIFICATE-----\n";
-
-// the following variables are unsigned longs because the time, measured in
-// milliseconds, will quickly become a bigger number than can be stored in an int.
-unsigned long lastTime = 0;
-// Timer set to 10 minutes (600000)
-//unsigned long timerDelay = 600000;
-// Set timer to 30 seconds (30000)
-unsigned long timerDelay = 30000;
-
+// Fonction pour transformer la donnée en Json
+template <typename t>
+String convertTOJson(t value, String type);
 void setup()
 {
     Serial.begin(9600);
     WiFi.begin(ssid, WPA2_AUTH_PEAP, EAP_IDENTITY, EAP_USERNAME, EAP_PASSWORD);
-    IPAddress dns1(8, 8, 8, 8);
-    Wifi.setDNS(dns1);
-    Serial.println("Connecting");
-    while(WiFi.status() != WL_CONNECTED)
+    Serial.println("Tentative de Connexion");
+    while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
         Serial.print(".");
+        counter++;
+        if(counter>=60)
+        {
+            Serial.println("Restart");
+            ESP.restart();
+        }
     }
     Serial.println("");
-    Serial.print("Connected to WiFi network with IP Address: ");
+    Serial.print("Connected to WiFi with IP : ");
     Serial.println(WiFi.localIP());
-    Serial.println("Timer set to 30 seconds (timerDelay variable), it will take 30 seconds before publishing the first reading.");
+    Serial.println("End of Setup");
 
     int status = WL_IDLE_STATUS;
 }
 void loop()
 {
-    StaticJsonDocument<200> file;
-    String content;
-    file["nom"] = "temp";
-    file["valeur"] = "19,2";
-    file["dateCapture"] = "2023-12-20 08:00:00";
-    file["localisation"] = "D004";
-    file["description"] = "";
-    file["nomsa"] = "13";
-    serializeJson(file, content);
+    HTTPClient https;
+    
 
-    String contentType = "text/html";
+    String contentType = "application/json";
     String postData = content;
     String dbname = "sae34bdm1eq1";
     String username = "m1eq1";
@@ -96,25 +56,15 @@ void loop()
         //Check WiFi connection status
         if(WiFi.status()== WL_CONNECTED)
         {
-            HttpClient https = HttpClient(Wifi,serverName,443);
-            Serial.println("making POST request");
-            https.beginRequest();
-            https.post(serverName);
-            https.sendHeader("accept",contentType);
-            https.sendHeader("dbname",dbname);
-            https.sendHeader("username",username);
-            https.sendHeader("userpass",userpass);
-            https.sendHeader("Content-Type", contentType);
-            https.beginBody();
-            https.print(postData);
-            https.endRequest();
-            // read the status code and body of the response
-            int statusCode = https.responseStatusCode();
-            String response = https.responseBody();
-            Serial.print("Status code: ");
-            Serial.println(statusCode);
-            Serial.print("Response: ");
-            Serial.println(response);
+            Serial.println("Sending POST request");
+            https.begin("https://sae34.k8s.iut-larochelle.fr/api/captures");
+            https.addHeader("accept",contentType);
+            https.addHeader("dbname",dbname);
+            https.addHeader("username",username);
+            https.addHeader("userpass",userpass);
+            https.addHeader("Content-Type", contentType);
+            int httpResponseCode = https.POST(postData);
+            Serial.println(https.errorToString(httpResponseCode));
         }
         else
         {
@@ -125,66 +75,18 @@ void loop()
     delay(1000);
 }
 
-/*
---------------------------
-Bazaar
---------------------------
-
-// JSON.typeof(jsonVar) can be used to get the type of the var
-            if (JSON.typeof(myObject) == "undefined")
-            {
-                Serial.println("Parsing input failed!");
-                return;
-            }
-            Serial.print("JSON object = ");
-            Serial.println(myObject);
-            // myObject.keys() can be used to get an array of all the keys in the object
-            JSONVar keys = myObject.keys();
-            for (int i = 0; i < keys.length(); i++)
-            {
-                JSO value = myObject[keys[i]];
-                Serial.print(keys[i]);
-                Serial.print(" = ");
-                Serial.println(value);
-                sensorReadingsArr[i] = double(value);
-            }
-            Serial.print("1 = ");
-            Serial.println(sensorReadingsArr[0]);
-            Serial.print("2 = ");
-            Serial.println(sensorReadingsArr[1]);
-            Serial.print("3 = ");
-            Serial.println(sensorReadingsArr[2]);
-*/
-/*
-"-----BEGIN CERTIFICATE-----\n" \
-"MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n" \
-"TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-"cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n" \
-"WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n" \
-"ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n" \
-"MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n" \
-"h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n" \
-"0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n" \
-"A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n" \
-"T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n" \
-"B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n" \
-"B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n" \
-"KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n" \
-"OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n" \
-"jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n" \
-"qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n" \
-"rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n" \
-"HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n" \
-"hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n" \
-"ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n" \
-"3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n" \
-"NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n" \
-"ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n" \
-"TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n" \
-"jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n" \
-"oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n" \
-"4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n" \
-"mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n" \
-"emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n" \
-"-----END CERTIFICATE-----\n"
-*/
+template <typename t>
+String convertTOJson(t value,String type)
+{
+    //Envoyer la valeur puis un string pui dit le type de la donnée. ex : "temp" ,"hum" ... et renvoie le json pour la requete http post
+    StaticJsonDocument<200> file;
+    String content;
+    file["nom"] = type;
+    file["valeur"] = "19.2";
+    file["dateCapture"] = "2023-12-20 08:00:00";
+    file["localisation"] = "D004";
+    file["description"] = "";
+    file["nomsa"] = "13";
+    serializeJson(file, content);
+    return content;
+}
